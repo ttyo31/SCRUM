d4af1b8765e5b0dc296ab93adcb85c28313aa233
ttyo31 <145460219+ttyo31@users.noreply.github.com>
2024-09-17 21:59:57 +0800

    frontend

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-card>
+    <v-tabs
+      v-model="tab"
+      bg-color="primary"
+    >
+    <router-link to="/"><v-tab value="one" class="text-white">Home</v-tab></router-link>
+      <router-link to="/about"><v-tab value="one" class="text-white">calendar</v-tab></router-link>
+      <v-tab value="three">Item Three</v-tab>
+    </v-tabs>
+  </v-card>
+  <v-app>
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  data: () => ({
+    //
+  }),
+}
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,61 @@
+
+  <template>
+  <!-- Ensure router-view is present to render pages -->
+  <v-app>
+    <v-main>
+      <v-container fluid style="padding: 0px;">
+      <v-col class="light-blue">
+          <!-- Overview Cards -->
+          <v-row style="padding: 0%;"> 
+            <v-col cols="9">
+              <v-card class="white pa-4">
+                <v-card-title class="text-blue" style="font-size: 100px;">Welcome, XXX</v-card-title>
+      
+                <v-card-subtitle class="text-blue" style="font-size: 30px;">You are currently working from XXX</v-card-subtitle>
+                <br>
+                <v-card-subtitle class="text-blue" style="font-size: 30px;">Number of Team Members in Office: xxxx</v-card-subtitle>
+                <br>
+                <v-card-subtitle class="text-blue" style="font-size: 30px;">Number of Team Members at Home: XXX</v-card-subtitle>
+                <br>
+                <v-card-subtitle class="text-blue" style="font-size: 30px;">You have xxx WFH days this week</v-card-subtitle>
+                <br>
+                <v-card-subtitle class="text-blue" style="font-size: 30px;">Upcoming WFH dates:</v-card-subtitle>
+              </v-card>
+            </v-col>
+            <v-col cols="3" style="padding-top: 0%;">
+              <v-card class="white pa-4">
+                <v-card-title class="text-blue" style="font-size: 20px; padding-bottom: 0%;">Apply for Flexible working</v-card-title>
+                <v-date-picker>
+                </v-date-picker>
+                <button>Submit</button>
+              </v-card>
+            </v-col>
+          </v-row>
+          <!-- Graphs and Charts -->
+          <!-- <v-row>
+            <v-col cols="12">
+              <v-card class="white pa-4">
+                <v-card-title class="text-blue">UpComing Flexible Working Dates</v-card-title>
+                
+              </v-card>
+            </v-col>
+          </v-row> -->
+        </v-col>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+
+<script>
+import { defineComponent } from 'vue';
+
+// Components
+
+export default defineComponent({
+  name: 'HomeView',
+
+  components: {
+  },
+});
+</script>

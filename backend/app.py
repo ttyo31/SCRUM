4ff4e511cae4e90642f7400d83a72ce8be9715ab
RegMCS <33059228+RegMCS@users.noreply.github.com>
2024-09-22 15:28:38 +0800

    Shifted manager application backend to app.py

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,21 @@
+import os
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
+from dotenv import load_dotenv
+from supabase import create_client, Client
+from flask_cors import CORS 
 
 app = Flask(__name__)
+CORS(app)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
+
+load_dotenv()
+
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_KEY")
+supabase: Client = create_client(url, key)
 
 
 class Employee(db.Model):
@@ -55,6 +66,17 @@ def list_employees():
         return jsonify({'success': True, 'count': count})
     except Exception as e:
         return f"Error retrieving employees: {e}", 500
+    
+
+@app.route('/WFHapplications/<mgr_id>', methods=['GET'])
+def get_applications(mgr_id):
+    '''
+    Returns existing WFH applications under a particular manager ID
+    '''
+    response = supabase.table('applications').select("*").eq("mgr_id", mgr_id).execute()
+    if response.data:
+        return jsonify(response.data), 200
+    return jsonify({"error": "No applications found"}), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
